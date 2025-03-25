c8c467425c125938857d4165fed3a88ea1bfe3b4
Edgar Luaces Roig <edgar_luaces@ceroca.cat>
2025-03-25 17:22:14 +0100

    Mostrar imagenes

--- lib/mongodb/db_conf.dart
+++ lib/mongodb/db_conf.dart
@@ -1,3 +1,3 @@
 class dbconf {
-  String connectstring ="mongodb+srv://edgarluaces:Culocagao@edgar.kkyq5.mongodb.net/?retryWrites=true&w=majority&appName=edgar";
+  String connectstring ="mongodb+srv://edgarluaces:Culocagao@edgar.kkyq5.mongodb.net/flutter_imagenes?retryWrites=true&w=majority&appName=edgar";
 }
--- lib/pagines/editardadesusuari.dart
+++ lib/pagines/editardadesusuari.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:exemple_firebase1/auth/servei_auth.dart';
 import 'package:exemple_firebase1/mongodb/db_conf.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -12,9 +15,8 @@ class Editardadesusuari extends StatefulWidget {
 }
 
 class _EditardadesusuariState extends State<Editardadesusuari> {
-
   mongodb.Db? _db;
-  Uint8List? _imageEnByets;
+  Uint8List? _imageEnBytes;
   final ImagePicker imagePicker = ImagePicker();
 
   @override
@@ -31,8 +33,7 @@ class _EditardadesusuariState extends State<Editardadesusuari> {
     _conectarAmbMongoDB().then((_) => print("conectats amb MongoBD"));
   }
 
-  Future _conectarAmbMongoDB() async{
-
+  Future _conectarAmbMongoDB() async {
     _db = await mongodb.Db.create(dbconf().connectstring);
 
     await _db!.open();
@@ -47,10 +48,79 @@ class _EditardadesusuariState extends State<Editardadesusuari> {
       body: Center(
         child: Column(
           children: [
-            Text("Edita les teves dades")
+            Text("Edita les teves dades"),
+
+            //afegim la imatge o un text
+            _imageEnBytes != null
+                ? Image.memory(
+                    _imageEnBytes!,
+                    height: 200,
+                  )
+                : const Text("No s'ha seleccionat cap imatge"),
+
+              const SizedBox(height: 20,),
+
+              ElevatedButton(onPressed: (){
+                _pujarImatge();
+              }, 
+              child: Text("Pujar imatge"),),
+
+              ElevatedButton(onPressed: () {
+                _recuperarImatge();
+              }, child: Text("recuperar imatge"),),
           ],
         ),
       ),
     );
+  }
+
+  Future _pujarImatge() async {
+    final imatgeseleccionada =
+        await imagePicker.pickImage(source: ImageSource.gallery);
+
+    //mirem si ha trobat una imatge
+    if (imatgeseleccionada != null) {
+      //per guardar la imatge hem de passar a bytes
+      final bytesImatge = await File(imatgeseleccionada.path).readAsBytes();
+      // passem els bytes en format que Mongodb li va be
+      final dadesBinaries = mongodb.BsonBinary.from(bytesImatge);
+
+      final collection = _db!.collection("Imatges_perfils");
+
+      await collection.replaceOne({
+        "id_usuari_firebase": ServeiAuth().getUsuariActual()!.uid
+      }, {
+        "id_usuari_firebase": ServeiAuth().getUsuariActual()!.uid,
+        "nom_foto": "foto_perfil",
+        "imatge": dadesBinaries,
+        "data_pujada": DateTime.now()
+      },
+          //fer que si no troba el document el crei
+          upsert: true);
+      print("imatge pujada");
+    }
+  }
+
+  Future<void> _recuperarImatge() async {
+    try {
+      //ens connectem amb la collection
+      final collection = _db!.collection("Imatges_perfils");
+
+      //trobem el document buscat
+      final doc = await collection
+          .findOne({"id_usuari_firebase": ServeiAuth().getUsuariActual()!.uid});
+
+      //podem recuperar imatge pero en format Bson
+      if (doc != null && doc["imatge"] != null) {
+        final imatgeBson = doc["imatge"] as mongodb.BsonBinary;
+        setState(() {
+          _imageEnBytes = imatgeBson.byteList;
+        });
+      } else {
+        print("Error trobant la imatge");
+      }
+    } catch (e) {
+      print("Error intentant recuperar el document");
+    }
   }
 }
